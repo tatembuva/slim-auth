9b3ee2a6fb63a7db7d6c48c326e20cd8f94017e9
Tate <tatembuva@hotmail.com>
2020-05-28 18:33:17 +0200

    ...initial setup, base slim project

---
+++ app/middleware.php
@@ -0,0 +1,12 @@
+<?php
+
+use Slim\App;
+
+return function(App $app){
+    $settings = $app->getContainer()->get('settings');
+    $app->addErrorMiddleware(
+        $settings['displayErrorDetails'],
+        $settings['logErrorDetails'],
+        $settings['logErrors']
+    );
+};
---
+++ bootstrap/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+$container = new Container;
+
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($container);
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$middleware = require __DIR__ . '/../app/middleware.php';
+$middleware($app);
+
+$app->get('/', function(Request $request, Response $response, $parameters){
+    $response->getBody()->write("Hello World");
+    return $response;
+});
+
+$app->run();
